5ca4a16c0c24aa12e4d0dfe9700c2e77b945afd8
Benjamin <f.nossin@rubika-edu.com>
2020-10-16 19:55:53 +0200

    cam bounds

--- SHMUP_PM_project/Assets/BAB/WUG_Scripts/Player_Screen_Limit.cs
+++ SHMUP_PM_project/Assets/BAB/WUG_Scripts/Player_Screen_Limit.cs
@@ -2,11 +2,24 @@
 
 public class Player_Screen_Limit : MonoBehaviour
 {
-    public BoxCollider2D screenLimit; 
+    public BoxCollider2D screenLimit;
+    private Vector2 screenBounds;
 
-    void FixedUpdate()
+    private float objectWidht; 
+    private float objectHeight; 
+
+    private void Start()
     {
-        transform.position = new Vector2(Mathf.Clamp(transform.position.x, screenLimit.bounds.min.x, screenLimit.bounds.max.x),
-                                         Mathf.Clamp(transform.position.y, screenLimit.bounds.min.y, screenLimit.bounds.max.y)); 
+        // screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidht = transform.GetComponent<SpriteRenderer>().bounds.size.x * 0.5f;
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y * 0.5f; 
+    }
+
+    private void LateUpdate() // otherwise it will glitch the sprite because clashing (Fixed)Update call
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenLimit.bounds.min.x + objectWidht, screenLimit.bounds.max.x - objectWidht);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenLimit.bounds.min.y + objectHeight, screenLimit.bounds.max.y - objectHeight);
+        transform.position = viewPos; 
     }
 }
